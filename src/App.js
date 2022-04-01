269f2228f72ef0f644d303733922e132e1d02818
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2022-04-01 15:23:43 -0700

    Created four components and added the to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import './App.css'
+import React from 'react'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import NewBlog from './components/NewBlog'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 const App = () => {
-  return <div className='App'></div>
+  return (
+    <div>
+      <Navbar />
+      <div className='content'>
+        <Router>
+          <Routes>
+            <Route path='/*' element={<Home />} />
+            <Route path='/create' element={<NewBlog />} />
+          </Routes>
+        </Router>
+      </div>
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/NewBlog.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewBlog = () => {
+  return (
+    <div>
+      <h1>Add New Blog</h1>
+    </div>
+  )
+}
+
+export default NewBlog
