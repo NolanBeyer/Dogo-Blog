63530c1294c9309f65b4e6bad65e8f10a275f578
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2022-04-02 02:28:24 -0700

    Created a custom Hook & used it in home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,16 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
 import BlogList from './BlogList'
+import useFetch from './useFetch'
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null)
-  const [isLoading, setIsLoading] = useState(true)
-
-  useEffect(() => {
-    fetch('http://localhost:4201/blogs')
-      .then((res) => {
-        return res.json()
-      })
-      .then((data) => {
-        setBlogs(data)
-        setIsLoading(false)
-      })
-      .catch((err) => {
-        console.log(err.message)
-      })
-  }, [])
-
+  const {
+    data: blogs,
+    error,
+    isLoading,
+  } = useFetch('http://localhost:4201/blogs')
   return (
     <div className='home'>
+      {error && <div>{error}</div>}
       {isLoading && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title={'All Blogs'} />}
     </div>
