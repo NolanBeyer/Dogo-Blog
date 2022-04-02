70f9a6082bcebad7fffcd5d1ea58d0c3f7fa3687
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2022-04-02 02:29:08 -0700

    Switched achor tags to Link tags

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const navbar = () => {
   return (
     <nav className='navbar'>
       <h1>The Dojo Blog</h1>
       <div className='links'>
-        <a href='/home'>Home</a>
-        <a href='/create'>New Blog</a>
+        <Link to='/home'>Home</Link>
+        <Link to='/create'>New Blog</Link>
       </div>
     </nav>
   )
