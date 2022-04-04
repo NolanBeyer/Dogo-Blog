d9f4b9576ec9713df02c25c6236635b53b52de56
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2022-04-04 01:22:58 -0700

    Finished create blog page

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,13 +1,30 @@
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Create = () => {
   const [title, setTitle] = useState('')
   const [body, setBody] = useState('')
   const [author, setAuthor] = useState('mario')
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const blog = { title, body, author }
+
+    fetch('http://localhost:4201/blogs/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      // history.go(-1);
+      navigate('/')
+    })
+  }
+
   return (
     <div className='create'>
-      <h2>Add New Blog</h2>
-      <form>
+      <h2>Add a New Blog</h2>
+      <form onSubmit={handleSubmit}>
         <label>Blog title:</label>
         <input
           type='text'
@@ -26,9 +43,6 @@ const Create = () => {
           <option value='yoshi'>yoshi</option>
         </select>
         <button>Add Blog</button>
-        <p>{title}</p>
-        <p>{body}</p>
-        <p>{author}</p>
       </form>
     </div>
   )
