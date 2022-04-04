9865dbad4c8a660dd6b9a586e969928976dcf59e
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2022-04-04 01:22:08 -0700

    Created not found page and added it to routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Create from './components/Create'
 import BlogDetails from './components/BlogDetails'
+import NotFound from './components/NotFound'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
             <Route path='/*' element={<Home />} />
             <Route path='/create' element={<Create />} />
             <Route path='/blogs/:id' element={<BlogDetails />} />
+            <Route path='*' element={<NotFound />} />
           </Routes>
         </div>
       </div>
