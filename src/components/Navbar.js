2bd14b5c75fc99033c7ed38ec1e30b18af387c50
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2022-04-04 01:23:29 -0700

    Fixed Link to home page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ const navbar = () => {
     <nav className='navbar'>
       <h1>The Dojo Blog</h1>
       <div className='links'>
-        <Link to='/home'>Home</Link>
+        <Link to='/'>Home</Link>
         <Link to='/create'>New Blog</Link>
       </div>
     </nav>
